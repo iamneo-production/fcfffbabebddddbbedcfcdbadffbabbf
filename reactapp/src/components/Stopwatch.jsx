8a8b0e6a4a50dfeb17733f69d45d5f2270a05cc7
Iamneo <techadmin@iamneo.ai>
2023-08-10 13:10:02 +0000

    Backup Commit

---
+++ reactapp/src/components/Stopwatch.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Stopwatch from './components/Stopwatch/Stopwatch';
+
+function App() {
+  return (
+    <div className="App">
+      <p style={{display:'none'}}>learn react link</p>
+      <Stopwatch />
+    </div>
+  );
+}
+
+export default App;
